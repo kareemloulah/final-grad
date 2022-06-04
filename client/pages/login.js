1f632176f79bf9168e65d6d2f4ced5cc8f73af41
kareemloulah <41597353+kareemloulah@users.noreply.github.com>
2022-06-05 00:30:30 +0200

    Revert "Update login.js"
    This reverts commit 789a0efb870b128f5a44c61addaaf5ace7d48d9b.

--- client/pages/login.js
+++ client/pages/login.js
@@ -10,17 +10,21 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
+
   // state
   const {
     state: { user },
     dispatch,
   } = useContext(Context);
   // const { user } = state;
+
   // router
   const router = useRouter();
+
   useEffect(() => {
     if (user !== null) router.push("/");
   }, [user]);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     // console.table({ name, email, password });
@@ -49,6 +53,7 @@ const Login = () => {
   return (
     <>
       <h1 className="jumbotron text-center bg-primary square">Login</h1>
+
       <div className="container col-md-4 offset-md-4 pb-5">
         <form onSubmit={handleSubmit}>
           <input
@@ -59,6 +64,7 @@ const Login = () => {
             placeholder="Enter email"
             required
           />
+
           <input
             type="password"
             className="form-control mb-4 p-4"
@@ -67,6 +73,7 @@ const Login = () => {
             placeholder="Enter password"
             required
           />
+
           <button
             type="submit"
             className="btn btn-block btn-primary"
@@ -75,12 +82,14 @@ const Login = () => {
             {loading ? <SyncOutlined spin /> : "Submit"}
           </button>
         </form>
+
         <p className="text-center pt-3">
           Not yet registered?{" "}
           <Link href="/register">
             <a>Register</a>
           </Link>
         </p>
+
         <p className="text-center">
           <Link href="/forgot-password">
             <a className="text-danger">Forgot password</a>
